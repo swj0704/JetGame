09a254370b2d30a9fe418c51f892b19ea919bd86
structsize <52183019+structsize@users.noreply.github.com>
2020-11-15 15:51:49 +0900

    [UPDATE] input background and player

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 MoveVector = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+    
+    void UpdateMove(){
+
+    }
+
+    public void PrecessInput(Vector3 moveDirection){
+
+    }
+}
